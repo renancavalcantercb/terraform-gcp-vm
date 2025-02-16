4bb215fb8d0455a002661b903806942c79063de3
Renan Cavalcante <renancavalcantercb@protonmail.com>
2025-02-16 05:13:21 -0300

    feat: change to ubuntu 22.04 lts

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-minimal-2004-lts"
+      image = "ubuntu-minimal-2204-lts"
     }
   }
 
